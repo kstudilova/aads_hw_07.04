a313bcd8bb78eb95f3feb93c7363c7ab359e16e2
kstudilova <kstudilova@gmail.com>
2026-04-13 21:34:58 +0300

    add structures

--- main.cpp
+++ main.cpp
@@ -1,4 +1,50 @@
 #include <iostream>
+
+template< class T, size_t K >
+struct BTree 
+{
+    T val[K];
+    BTree< T, K >* childs[K + 1];
+    BTree< T, K >* parent;
+};
+
+template< class T, size_t K >
+struct BTreeIt
+{
+    size_t s;
+    BTree< T, K >* current;
+};
+
+template< class T, size_t K >
+T value(BTreeIt< T, K > it)
+{
+
+}
+
+template< class T, size_t K >
+BTreeIt< T, K > next(BTreeIt< T ,K > it)
+{
+
+}
+
+template< class T, size_t K >
+BTreeIt< T, K > prev(BTreeIt< T ,K > it)
+{
+    
+}
+
+template< class T, size_t K >
+bool hasNext(BTreeIt< T ,K > it)
+{
+    
+}
+
+template< class T, size_t K >
+bool hasPrev(BTreeIt< T ,K > it)
+{
+    
+}
+
 int main()
 {
   
