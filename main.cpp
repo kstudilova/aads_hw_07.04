484eb6def5290836ff51bb8ad0ca09842cb64e63
kstudilova <kstudilova@gmail.com>
2026-04-13 21:52:12 +0300

    add value and maximum and minimum functions

--- main.cpp
+++ main.cpp
@@ -3,22 +3,44 @@
 template< class T, size_t K >
 struct BTree 
 {
-    T val[K];
-    BTree< T, K >* childs[K + 1];
-    BTree< T, K >* parent;
+  T val[K];
+  BTree< T, K >* childs[K + 1];
+  BTree< T, K >* parent;
 };
 
 template< class T, size_t K >
 struct BTreeIt
 {
-    size_t s;
-    BTree< T, K >* current;
+  size_t s;
+  BTree< T, K >* current;
 };
+
+template<class T, size_t K>
+BTreeIt<T, K> minimum(BTree<T, K>* node)
+{
+  if (!node) return {0, nullptr};
+  while (node->children[0]) {
+    node = node->children[0];
+  }
+  return {0, node};
+}
+
+template<class T, size_t K>
+BTreeIt<T, K> maximum(BTree<T, K>* node)
+{
+  if (!node) return {0, nullptr};
+  while (node->children[K]) {
+    node = node->children[K];
+  }
+  size_t last = K - 1;
+  while (last > 0 && node->val[last] == T()) last--;
+  return {last, node};
+}
 
 template< class T, size_t K >
 T value(BTreeIt< T, K > it)
 {
-
+  return it.current->val[it.s];
 }
 
 template< class T, size_t K >
@@ -36,7 +58,7 @@ BTreeIt< T, K > prev(BTreeIt< T ,K > it)
 template< class T, size_t K >
 bool hasNext(BTreeIt< T ,K > it)
 {
-    
+
 }
 
 template< class T, size_t K >
